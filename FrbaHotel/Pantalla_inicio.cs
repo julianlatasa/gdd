96407cbed2086fd47d9a1c1a8927d95d62145027
centenonicolas <centeno.nico@gmail.com>
2018-04-30 17:34:58 -0300

    ADD: Agrego modelo de la app Desktop proveida por la catedra y pantalla de inicio.

---
+++ FrbaHotel/Pantalla_inicio.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrbaHotel
+{
+    public partial class Pantalla_inicio : Form
+    {
+        public Pantalla_inicio()
+        {
+            InitializeComponent();
+        }
+
+        //ACCION CLICK EN "INGRESAR COMO USUARIO"
+        private void user_button_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        //ACCION CLICK EN "INGRESAR COMO INVITADO"
+        private void guest_button_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
