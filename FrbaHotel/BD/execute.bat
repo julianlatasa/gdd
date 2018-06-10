d8ba9dc6170aa22c446fb7eee6dd86b21e9bf975
Sebastian Plawner <sebastian.plawner@gmail.com>
2018-06-10 02:06:43 -0300

    Cambios scripts BD En create_sp saqué los DROP PROCEDURE porque no funcionaban, hay que hacerlo con el otro script. Corregí los errores para cargar los SP, eran valores faltantes en los insert. Queda ver la migración, revisar tabla por tabla si los datos migrados tienen consistencia. Encontré errores en columnas en las tablas Facturas, Hoteles, Clientes, Linea Facturas. El script drop ahora dropea primero todas las constraints. El archivo drop_all dropea todo, incluso la master, ojo. Saqué el archivo resultado_output y lo mandé al gitignore. Agregué el nombre de los archivos al batch, porque paja pasarlo por parámetro.

--- FrbaHotel/BD/execute.bat
+++ FrbaHotel/BD/execute.bat
@@ -1,5 +1,4 @@
 @echo off
 set host=%computername%
-set file=%1%.sql
-sqlcmd -S %host%\SQLEXPRESS -U gdHotel2018 -P gd2018 -i %file% -o resultado_output.txt
+sqlcmd -S %host%\SQLEXPRESS -U gdHotel2018 -P gd2018 -i gd_esquema.Schema.sql,gd_esquema.Maestra.Table.sql -o resultado_output.txt
 type resultado_output.txt
